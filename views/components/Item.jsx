81291a741febcd2fbc00e8bc99e0955db3ca4c1e
Zach Posten <zposten@gmail.com>
2017-09-25 22:45:48 -0500

    feat: Add hover handle to todo items
    This will be used for dragging the items to reorder them

--- views/components/Item.jsx
+++ views/components/Item.jsx
@@ -3,6 +3,13 @@
 import React from 'react';
 
 export default class Item extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isHovered: false
+    }
+  }
+
   renderButtons() {
     let pauseBtn = (
       <button className="pause" onClick={() => this.props.onPause(this.props.item)}></button>
@@ -11,17 +18,35 @@ export default class Item extends React.Component {
     return (
       <div className="buttons">
         <button className="delete" onClick={() => this.props.onDelete(this.props.item)}></button>
-        { this.props.paused ? null : pauseBtn }
+        {this.props.paused ? null : pauseBtn}
         <button className="complete" onClick={() => this.props.onComplete(this.props.item)}></button>
       </div>
     );
   }
 
+  onHover(isHovered) {
+    this.setState({isHovered})
+  }
+
   render() {
+    let hovered = (this.state.isHovered ? 'hovered' : '');
+
     return (
-      <div className="item">
-        <div className="item-name">{this.props.text}</div>
-        {this.renderButtons()}
+      <div className="item-wrapper" 
+           onMouseEnter={() => this.onHover(true)}
+           onMouseLeave={() => this.onHover(false)}>
+        <div className={"item-handle " + hovered}>
+          <div className="handle-dot"/>
+          <div className="handle-dot"/>
+          <div className="handle-dot"/>
+          <div className="handle-dot"/>
+          <div className="handle-dot"/>
+          <div className="handle-dot"/>
+        </div>
+        <div className="item">
+          <div className="item-name">{this.props.text}</div>
+          {this.renderButtons()}
+        </div>
       </div>
     );
   }
